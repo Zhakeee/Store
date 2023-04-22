262a55a2a1737df504d914e4298d4facf3cb73a6
Aibek Katenov <a_katenov@kbtu.kz>
2023-04-22 18:20:20 +0600

    front

---
+++ endterm_front/src/app/services/product.resolver.ts
@@ -0,0 +1,25 @@
+import { ProductsService } from './products.service';
+import { IProducts } from '../models/products';
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { catchError, EMPTY, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductResolver implements Resolve<IProducts> {
+  constructor(private ProductsService: ProductsService, private router: Router) { }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProducts> {
+    return this.ProductsService.getProduct(route.params?.['id']).pipe(
+      catchError(() => {
+        this.router.navigate(['products']);
+        return EMPTY;
+      })
+    );
+  }
+}
